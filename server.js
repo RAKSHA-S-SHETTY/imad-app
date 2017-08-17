f47a045100769543cffc54248b0c1c0629d7eb06
RAKSHA-S-SHETTY <31028474+RAKSHA-S-SHETTY@users.noreply.github.com>
2017-08-17 22:06:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/artical-one',function (req,res) {
+app.get('/artical-one', function (req, res) {
     res.send('artical one will be served here ');
     
 });
-app.get('/artical-two',function (req,res){
+app.get('/artical-two' , function (req, res){
     res.send('artical two will be served here ');
     
 });
-app.get('/artical-three',function (req,res) {
+app.get('/artical-three' , function (req, res) {
     res.send('artical three will be served here ');
     
 });
